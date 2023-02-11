7d58d12e85b3200400277949ce37aa22cdca0438
宿命 <45293856+sumingyd@users.noreply.github.com>
2023-02-12 05:21:32 +0800

    fix

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -25,7 +25,7 @@ module.exports = {
         '/': {
             lang: 'zh-CN',
             title: 'OpenCore安装指南',
-            description: '中文版OpenCore安装指南'
+            description: '中文版OpenCore安装指南 - Ver.0.8.8'
         },
         '/zh/': {
             lang: 'en-US',
